e39ed290ca0ef3e6909f0c1d43b08a61451c9709
Kirk Santizo <kisantizo@bi.com.gt>
2022-01-31 12:29:56 -0600

    feat(config route): config validation page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { PostPageComponent } from './pages/post-page/post-page.component';
 import { FormsModule } from '@angular/forms';
+import { ValidationComponent } from './pages/validation/validation.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  {path: 'home', component: HomeComponent },
-  {path: 'post', component: PostPageComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'validation', component: ValidationComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,20 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostComponent } from './components/post/post.component';
 import { ErrorInterceptorService } from '@/app/utils/error-interceptor.service';
 import { HomeComponent } from './pages/home/home.component';
 import { errorReducer } from './store/error.reducer';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ValidationComponent } from './pages/validation/validation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
-    HomeComponent
+    HomeComponent,
+    ValidationComponent
   ],
   imports: [
     BrowserModule,
