af1f32914643f2d976bcec1e1907d46cbdcb7d62
Maiver Alejandro Pichola Gua <61585@bi-central.local>
2022-02-08 16:02:15 -0600

    refactor(add validation): adding blocked in form inputs

--- src/app/pages/validation/validation.component.ts
+++ src/app/pages/validation/validation.component.ts
@@ -6,33 +6,60 @@ import { UserInterface } from '@/app/interfaces/user';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { InputType } from '@/app/types/inputType';
 import { AppState } from '@/app/app.state';
-import * as UserAction from '@/app/store/user.actions'
+import * as UserAction from '@/app/store/user.actions';
 @Component({
   selector: 'app-validation',
   templateUrl: './validation.component.html',
-  styleUrls: ['./validation.component.scss']
+  styleUrls: ['./validation.component.scss'],
 })
 export class ValidationComponent implements OnInit {
-  form!: FormGroup
-  constructor(private formBuilder: FormBuilder, private store: Store<AppState>, private router: Router) { }
+  form!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private store: Store<AppState>,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-      this.createForm();
+    this.createForm();
   }
 
-  createForm():void {
+  createForm(): void {
     this.form = this.formBuilder.group({
-      name: ['', [Validators.required]],
-      lastname: ['', [Validators.required]],
-      dpi: ['', [Validators.required, , Validators.pattern(InputType.number)]],
-      phone: ['', [Validators.required, Validators.pattern(InputType.number)]],
-      email: ['', [Validators.required, , Validators.pattern(InputType.email)]]
-    })
+      name: ['', [Validators.required, Validators.maxLength(50)]],
+      lastname: ['', [Validators.required], Validators.maxLength(50)],
+      dpi: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(InputType.number),
+          Validators.minLength(13),
+          Validators.maxLength(13),
+        ],
+      ],
+      phone: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(InputType.number),
+          Validators.minLength(8),
+          Validators.maxLength(8),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(InputType.email),
+          Validators.maxLength(50),
+        ],
+      ],
+    });
   }
 
   nextStep() {
-    const payload:UserInterface = {...this.form.value, photo:''}
-    this.store.dispatch(UserAction.ADD_USER({ payload }))
+    const payload: UserInterface = { ...this.form.value, photo: '' };
+    this.store.dispatch(UserAction.ADD_USER({ payload }));
     this.router.navigate(['selfie']);
   }
 }
--- src/app/types/inputType.ts
+++ src/app/types/inputType.ts
@@ -1,5 +1,6 @@
-export enum InputType {
-  string = '.',
-  number = '^[0-9]+$',
-  email = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
+export const InputType = {
+  all: /./g,
+  string: /^(([A-záéíóúñÁÉÍÓÚÑ])|([a-z])+( ))+$/g,
+  number: /^[0-9]+$/,
+  email: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/g
 }
